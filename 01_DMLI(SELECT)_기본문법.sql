d89c7e40f86e7e2ffd9c3bb4c663681c8dcc9ecd
ohy794 <ohy794@naver.com>
2022-12-13 15:16:55 +0900

    기본문법 / 함수
    1

--- 01_DMLI(SELECT)_기본문법.sql
+++ 01_DMLI(SELECT)_기본문법.sql
@@ -28,7 +28,7 @@ EMP_NAME,
 email,
 PHONE
 FROM EMPLOYEE;
-
+ 
 ------------------------------실습문제---------------------------------
 -- 1번 job테이블의 모든칼럼 조회
 SELECT *
@@ -146,10 +146,275 @@ FROM EMPLOYEE;
     != , ^=,  <>일치하지 않는가.
 */
 
+-- EMPLOYEE 테이블에서 급여가 400만원 이상인 사원들만 조회
+SELECT *
+FROM EMPLOYEE
+WHERE SALARY >= 4000000;
 
 
+-- EMPLOYEE 테이블로 부터 부서코드가 D9인 사원들의 사원명 , 부서코드 급여 조회
+SELECT EMP_NAME , DEPT_CODE , SALARY
+FROM EMPLOYEE 
+WHERE DEPT_CODE = 'D9';
+
+-- EMPLOYEE 테이블로 부터 부서코드가 D9이 아닌 사원들의 사원명 부서코드 급여조회
+SELECT EMP_NAME , DEPT_CODE , SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE<>'D9'; 
+--DEPT_CODE ^='D9'; 
+--DEPT_CODE != 'D9';
+
+----------------------- 실습 문제-------------------------------------
+
+--1. EMPLOTEE 테이블에서 급여가 300만원 이상인 사원들의 이름 급여 입사일 조회
+SELECT EMP_NAME , SALARY , HIRE_DATE
+FROM EMPLOYEE
+WHERE SALARY >= 3000000;
+
+--2. EMPLOYEE 테이블에서 직급코드가 J2인사원들의 이름 급여 보너스 조회
+SELECT EMP_NAME , JOB_CODE , SALARY , BONUS
+FROM EMPLOYEE
+WHERE JOB_CODE  = 'J2';
+
+--3. EMPLOYEE 테이블에서 현재 재직주인 사원들의 사번, 이름 , 입사일조회
+
+SELECT EMP_ID , EMP_NAME , HIRE_DATE
+FROM EMPLOYEE
+WHERE ENT_YN = 'N';
+
+--4. EMPLOYEE 테이블에서 연봉(급여*12)이 5000만원 이상인 사원들의 이름, 급여 ,연봉 입사일조회
+
+SELECT EMP_NAME , SALARY ,   SALARY * 12 AS "연봉"   , HIRE_DATE
+FROM EMPLOYEE
+WHERE SALARY * 12 >= 50000000;
+--SELECT절에서 부여한 별칭은 WHERE절에서 사용할수 없다.
+
+/*
+    <논리연산자>
+    여러개의 조건을 엮을때 사용
+    AND(자바 :&&) , OR(자바 :||)
+    AND : ~이면서 , 그리고
+    OR : ~이거나 , 또는
+*/
+
+-- EMPLOYEE 테이블에서 부서코드가 D9이면서 급여가 500만원 이상인 사람들의 이름, 부서코드 ,급여조회
+SELECT 
+    EMP_NAME, DEPT_COME, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D9' AND SALARY >= 5000000;
+
+-- 부서코드가 D6이거나 급여가 300만원 이상이면서 재직중인 사람들의 이름 부서코드 급여조회
+SELECT 
+    EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE (DEPT_CODE = 'D6' OR SALARY >= 3000000) AND ENT_YN = 'N';
+
+-- 급여가 350만원 이상이고 600만원 이하인 사원들의 이름 사번 급여 직급코드조회
+SELECT 
+    EMP_NAME, EMP_ID, SALARY , JOB_CODE 
+FROM EMPLOYEE
+WHERE 3500000 <= SALARY AND SALARY <= 6000000;
+
+/*
+    <BETWEEN AND>
+    몇 이상 몇 이하인 범위에 대한 조건을 제시할때 사용
+*/
+-- 급여가 350만원 이상이고 600만원 이하인 사원들의 이름 사번 급여 직급코드조회
+SELECT 
+    EMP_NAME, EMP_ID, SALARY , JOB_CODE 
+FROM EMPLOYEE
+WHERE SALARY BETWEEN 3500000 AND 6000000; 
+
+-- 급여가 350만원 미만이고, 600만원 초과인 사원들의 이름, 사번, 급여 직급코드 조회
+
+SELECT 
+    EMP_NAME, EMP_ID, SALARY , JOB_CODE 
+FROM EMPLOYEE
+WHERE  
+    --SALARY < 3500000 OR SALARY >6000000; 
+    SALARY NOT BETWEEN 3500000 AND 6000000; 
+    -- 오라클의 NOT은 자바 논리부정연산자와 동일함.
+
+-- ** BETWEEN가지고 DATE 형식간에도 비교가능
+-- 입사일이 '90/01/01' ~ '03/01/01' 인 사원들의 모든 칼럼 조회;
+SELECT *
+FROM EMPLOYEE
+WHERE 
+--HIRE_DATE >= '90/01/01' AND HIRE_DATE <= '03/01/01';
+    HIRE_DATE BETWEEN '90/01/01' AND '03/01/01';
+
+/*
+    <LIKE '특정패턴'>
+    비교하고자 하는 칼럼의 값이 내가 지정한 특정 패턴에 만족될 경우 조회
+    
+    [표현법]
+    칼럼명 LIKE '특정패턴'
+    
+    - 옵션 : 특정패턴 부분에 와일드카드인 '&' , '_' 를 가지고 제시할수 있음
+    '%' : 0글자
+          비교대상 칼럼이 LIKE '문자%' => 칼럼값중에 '문자'로 시작되는 것을 조회
+          비교대상 칼럼명 LIKE '%문자' => 칼럼값중에 '문자'로 끝나는 것을 조회
+          비교대상 칼럼명 LIKE '%문자%' => 칼럼값중에 '문자'가 포함되는것을 조회
+          
+    '_' : 1글자
+          비교대상 칼럼명 LIKE '_문자' => 칼럼값중에 '문자' 앞에 무조건 1글자가 존재하는 경우 조회
+          비교대상 칼럼명 LIKE '__문자' => 칼럼값중에 '문자' 앞에 무조건 2글자가 존재하는 경우 조회
+    
+*/
+
+-- 성이 전씨인 사원들의 이름 , 급여, 입사일 조회
+SELECT EMP_NAME , SALARY , HIRE_DATE
+FROM EMPLOYEE
+WHERE EMP_NAME LIKE '전%';
+
+-- 이름 중에 '하' 가 포함된 사원들의 이름, 주민번호, 부서코드조회
+SELECT EMP_NAME , EMP_NO , DEPT_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME LIKE '%하%';
 
 
+-- 전화번호 4번째 자리가 9로 시작하는 사원들의 사번, 사원명 전화번호, 이메일 조회
+SELECT EMP_ID , EMP_NAME , PHONE , EMAIL 
+FROM EMPLOYEE
+WHERE PHONE LIKE '___9%';
 
+
+-- 이름 가운데 글자가 '지' 인 사원들의 모든 칼럼 
+SELECT *
+FROM EMPLOYEE
+WHERE EMP_NAME LIKE '_지_';
+
+---------------------- 실습문제 ------------------------------------
+-- 1.이름이 '연' 으로 끝나는 사원들의 이름 입사일조회
+SELECT EMP_NAME , HIRE_DATE
+FROM EMPLOYEE
+WHERE EMP_NAME LIKE '__연';
+
+
+-- 2. 전화번호 처음 3글자가 010이 아닌 사원들의 이름, 전화번호조회
+SELECT EMP_NAME , PHONE
+FROM EMPLOYEE
+WHERE PHONE NOT LIKE '010%';
+
+
+-- 3. DEPARTMENT 테이블에서 해외영업과 관련된 부서들의 모든 컬럼조회
+SELECT *
+FROM DEPARTMENT 
+WHERE DEPT_TITLE LIKE '해외영업%';
+
+/*
+    <IS NULL>
+    해당 값이 NULL인지 비교해주는 구문.
+    
+    [표현법]
+    비교대상칼럼 IS NULL : 컬럼값이 NULL인경우
+    비교대상칼럼 IS NOT NULL : 컬럼값이 NULL이 아닌 경우
+*/
+
+-- 보너스를 받지 않는 사원들(BONUS 칼럼의 값이 NULL) 사번, 이름, 급여 보너스
+SELECT EMP_ID, EMP_NAME, SALARY , BONUS
+FROM EMPLOYEE
+WHERE BONUS IS NULL;
+
+--사수가 없는 사원들의 사원명, 사수번호, 부서코드 조회
+SELECT EMP_NAME , MANAGER_ID , DEPT_CODE
+FROM EMPLOYEE
+WHERE MANAGER_ID IS NULL;
+
+-- 사수도 없고 부서도 배치받지 못한 사원들의 모든칼럼 조회
+SELECT *
+FROM EMPLOYEE
+WHERE MANAGER_ID IS NULL AND DEPT_CODE IS NULL;
+
+-- 부서배치는 받지 않았지만 보너스는 받는 사원의 모든칼럼조회
+SELECT *
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NULL AND BONUS IS NOT NULL;
+
+/*
+    <IN>
+    비교 대상 칼럼 값에 내가 제시한 목록들중 일치하는값이 있는지 판단
+    
+    [표현법] 
+    칼럼명 IN(값, 값, 값, 값,...)
+*/
+
+-- 부서코드가 D6이거나 또는 D8이거나 또는 D5인 사원들의 이름, 부서코드, 급여조회
+SELECT EMP_NAME , DEPT_CODE , SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE IN ('D6', 'D8' , 'D5');
+
+-- 직급코드가 J1 , J3 , J4가 아닌 사원들의 모든 칼럼 조회
+SELECT *
+FROM EMPLOYEE
+WHERE JOB_CODE NOT IN ('J1' , 'J3' , 'J4');
+
+/*
+    <연결연산자 ||>
+    여러 컬럼값들을 마치 하나의 컬럼인 것 처럼 연결시켜주는 연산자
+    컬럼과 리터럴(임의의 문자열값등)을 연결할 수 있음.
+    
+    자바:System.out.println("num : "+num);
+*/
+
+
+SELECT EMP_ID || EMP_NAME || SALARY AS "연결"
+FROM EMPLOYEE;
+
+-- XX번 XXX의 월급은 XXXX원 입니다 AS 급여정보
+SELECT EMP_ID || '번 '|| EMP_NAME || '의 월급은' || SALARY || '원 입니다.' AS 급여정보
+FROM EMPLOYEE;
+
+
+/*
+    <연산자 우선순위>
+    0. ()
+    1. 산술연산자
+    2. 연결연산자
+    3. 비교연산자
+    4. IS NULL , LIKE , IN
+    5. BETWEEN AND
+    6. NOT
+    7. AND
+    8. OR 
+    
+    -------------------------------------------------------------------------
+    
+    <ORDER BY 절>
+    SELECT 문 가장 마지막에 가입하는 구문뿐 만 아니라 가장 마지막에 실행되는구문
+    최종 조회된 결과물들에 대해서 정렬 기준을 세워주는 구문
+    
+    [표현법]
+    SELECT 컬럼명1 , 컬럼명2,
+    FROM 테이블명
+    WHERE 조건식(생략가능)
+    ORDER BY [정렬기준으로 세우고자 하는 칼럼명/별칭/컬럼순번] [ASC/DESC] (생략가능) [NULLS FIRST/ NULLS LAST] (생략가능)
+    
+    
+    
+*/
+
+-- 월급이 높은 사람들부터 나열하고 싶다
+SELECT *
+FROM EMPLOYEE 
+ORDER BY SALARY DESC;
+
+-- 보너스 기준 정렬
+SELECT *
+FROM EMPLOYEE
+--ORDER BY BONUS; -- ASC(오름차순) && NULLS LAT 요두개가 기본값.
+--ORDER BY BONUS ASC NULLS FIRST;
+--ORDER BY BONUS DESC; --NULLS FIRST 기본값.
+ORDER BY BONUS DESC, SALARY ASC;
+--> 첫번쨰로 제시한 정렬기준 컬럼값이 일치할 경우 두번째 정렬기준을 가지고 다시 정렬.
+
+-- 연본기준 줄세우기
+SELECT EMP_NAME , SALARY, SALARY *12 AS 연봉
+FROM EMPLOYEE
+--ORDER BY 연봉 DESC;
+--ORDER BY(SALARY*12) DESC;
+ORDER BY 3;
+
+--> ORDER BY 절은 숫자뿐만아니라 문자열, 날짜에 대해서도 정렬가능하다
 
 
